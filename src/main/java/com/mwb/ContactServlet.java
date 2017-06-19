5f8de70ea477bd3c77e916e28ec6591b976681b2
mengweibo <mengweibo@shbj.com>
2017-06-19 15:37:39 +0800

    [mwb]servlet

---
+++ src/main/java/com/mwb/ContactServlet.java
@@ -0,0 +1,52 @@
+package com.mwb;
+
+import java.io.IOException;
+import java.sql.*;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Created by MengWeiBo on 2017-06-19
+ */
+public class ContactServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        Connection conn = null;
+        String sql;
+        String url = "jdbc:mysql://localhost:3306/resume?"
+                + "user=root&password=p@swrd123&useUnicode=true&characterEncoding=UTF8";
+        try {
+            String name = req.getParameter("name");
+            String email = req.getParameter("email");
+            String content = req.getParameter("content");
+
+            Class.forName("com.mysql.jdbc.Driver");// 动态加载mysql驱动
+            System.out.println("成功加载MySQL驱动程序");
+            conn = DriverManager.getConnection(url);
+            Statement stmt = conn.createStatement();
+            sql = "INSERT INTO `t_contact`(`name`,`email`,`content`) VALUES ('" + name + "', '" + email + "', '" + content + "');";
+            int result = stmt.executeUpdate(sql);// executeUpdate语句会返回一个受影响的行数，如果返回-1就没有成功
+            System.out.println(result);
+        } catch (SQLException e) {
+            System.out.println("MySQL操作错误");
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            resp.sendRedirect("index.html");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        doGet(req, resp);
+    }
+}
